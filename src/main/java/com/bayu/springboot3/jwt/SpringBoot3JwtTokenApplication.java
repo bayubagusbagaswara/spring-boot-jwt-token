e10d4e0da5a7a8aa26cc3be0d0f368e6b55bb901
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-13 12:13:52 +0700

    Initial commit

---
+++ src/main/java/com/bayu/springboot3/jwt/SpringBoot3JwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.springboot3.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3JwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/springboot3/jwt/SpringBoot3JwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.springboot3.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
