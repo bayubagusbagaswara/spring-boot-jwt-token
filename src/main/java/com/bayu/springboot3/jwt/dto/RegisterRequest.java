2bdb3f985168992badf01585ca1ff122033b5775
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-22 21:02:49 +0700

    add dto RegisterRequest

---
+++ src/main/java/com/bayu/springboot3/jwt/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.bayu.springboot3.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
