984f4c5ae873d3b763be0d03727918da06ea2a2b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-22 21:03:50 +0700

    add dto AuthenticationRequest

---
+++ src/main/java/com/bayu/springboot3/jwt/dto/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.bayu.springboot3.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+
+    private String password;
+}
