88419c8996eed6752fb9a2228ebf671d1712bd27
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 09:50:13 +0700

    inject TokenRepository

--- src/main/java/com/bayu/springboot3/jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/bayu/springboot3/jwt/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.bayu.springboot3.jwt.config;
 
+import com.bayu.springboot3.jwt.repository.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,6 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
