ed9235b31c1546f01254c39674cd9fbe405d1cd1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 10:13:17 +0700

    implement method logout

--- src/main/java/com/bayu/springboot3/jwt/service/impl/LogoutServiceImpl.java
+++ src/main/java/com/bayu/springboot3/jwt/service/impl/LogoutServiceImpl.java
@@ -1,5 +1,7 @@
 package com.bayu.springboot3.jwt.service.impl;
 
+import com.bayu.springboot3.jwt.model.Token;
+import com.bayu.springboot3.jwt.repository.TokenRepository;
 import com.bayu.springboot3.jwt.service.LogoutService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,11 +13,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogoutServiceImpl implements LogoutService {
 
+    private final TokenRepository tokenRepository;
+
     @Override
     public void logout(
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication) {
-        
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        Token storedToken = tokenRepository.findByTokenValue(jwt)
+                .orElse(null);
+
+        if (storedToken != null) {
+            storedToken.setExpired(Boolean.TRUE);
+            storedToken.setRevoked(Boolean.TRUE);
+            tokenRepository.save(storedToken);
+        }
     }
+
 }
