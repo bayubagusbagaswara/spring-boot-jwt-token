01c868bfab04ba6907d224b6376bcff13063d09a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 11:50:45 +0700

    add value refresh expiration

--- src/main/java/com/bayu/springboot3/jwt/config/JwtService.java
+++ src/main/java/com/bayu/springboot3/jwt/config/JwtService.java
@@ -24,6 +24,9 @@ public class JwtService {
     @Value("${application.security.jwt.expiration}")
     private long jwtExpiration;
 
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
