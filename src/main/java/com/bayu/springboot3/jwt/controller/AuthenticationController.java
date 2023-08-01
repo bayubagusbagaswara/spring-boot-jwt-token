44d4ad22cfed1713ece771e6a94df9746066b828
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 12:17:08 +0700

    add method refreshToken

--- src/main/java/com/bayu/springboot3/jwt/controller/AuthenticationController.java
+++ src/main/java/com/bayu/springboot3/jwt/controller/AuthenticationController.java
@@ -4,12 +4,16 @@ import com.bayu.springboot3.jwt.dto.AuthenticationRequest;
 import com.bayu.springboot3.jwt.dto.AuthenticationResponse;
 import com.bayu.springboot3.jwt.dto.RegisterRequest;
 import com.bayu.springboot3.jwt.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "/api/v1/auth")
@@ -31,4 +35,13 @@ public class AuthenticationController {
     ) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
+
+    @PostMapping(path = "/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        authenticationService.refreshToken(request, response);
+    }
+
 }
