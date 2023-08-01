b06fd195e91f07b99f1feb5c0b7dc3d1186fc057
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 09:14:16 +0700

    add method findAllValidTokensByUser

---
+++ src/main/java/com/bayu/springboot3/jwt/repository/TokenRepository.java
@@ -0,0 +1,18 @@
+package com.bayu.springboot3.jwt.repository;
+
+import com.bayu.springboot3.jwt.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    @Query("""
+            SELECT t FROM Token t INNER JOIN User u ON t.user.id = u.id
+            WHERE u.id = :userId AND (t.expired = false OR t.revoked = false)
+            """)
+    List<Token> findAllValidTokensByUser(Integer userId);
+}
