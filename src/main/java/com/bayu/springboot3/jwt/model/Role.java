9a5907513affa1ea74e54dc661f19a07bd6f35be
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 15:51:29 +0700

    add role and permission

--- src/main/java/com/bayu/springboot3/jwt/model/Role.java
+++ src/main/java/com/bayu/springboot3/jwt/model/Role.java
@@ -1,7 +1,54 @@
 package com.bayu.springboot3.jwt.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static com.bayu.springboot3.jwt.model.Permission.*;
+
+@RequiredArgsConstructor
 public enum Role {
 
-    USER,
-    ADMIN
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_CREATE,
+                    ADMIN_DELETE,
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_CREATE,
+                    MANAGER_DELETE
+            )
+    ),
+    MANAGER(
+            Set.of(
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_CREATE,
+                    MANAGER_DELETE
+            )
+    )
+
+    ;
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>(getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(
+                        permission.getPermission()
+                ))
+                .toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+
 }
