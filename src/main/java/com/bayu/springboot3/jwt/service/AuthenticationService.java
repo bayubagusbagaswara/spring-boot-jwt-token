07e5804e50a24f444187ca8247c99893b82d61bd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 12:16:37 +0700

    add method refreshToken

--- src/main/java/com/bayu/springboot3/jwt/service/AuthenticationService.java
+++ src/main/java/com/bayu/springboot3/jwt/service/AuthenticationService.java
@@ -3,10 +3,16 @@ package com.bayu.springboot3.jwt.service;
 import com.bayu.springboot3.jwt.dto.AuthenticationRequest;
 import com.bayu.springboot3.jwt.dto.AuthenticationResponse;
 import com.bayu.springboot3.jwt.dto.RegisterRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public interface AuthenticationService {
 
     AuthenticationResponse register(RegisterRequest request);
 
     AuthenticationResponse authenticate(AuthenticationRequest request);
+
+    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
