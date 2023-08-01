fe332628fd0c230b574036d993c5633a05a3bf9e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 13:52:37 +0700

    add AdminController

---
+++ src/main/java/com/bayu/springboot3/jwt/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.bayu.springboot3.jwt.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+}
